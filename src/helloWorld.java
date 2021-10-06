f3edf5aaaa4c3abf47091c62b6596c9ef8b86026
NitaWasniowski <nita.wasniowski@brainsphere.de>
2021-10-06 17:14:51 +0200

    Zahlen Ausgabe ist neu

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,8 +1,14 @@
 
 public class HelloWorld {
-  public static void main(String[] args) {
-	  System.out.println("hallo");
-    System.out.println("Anderer Text"); 
+	public static void main(String[] args) {
+		
+		int[] zahlen = {1,2,3,4,5,6,7,8,9} ; 	
+		
+		System.out.println("hallo World");
+		
+		for( int element : zahlen ){
+			System.out.println( "Dies war in dem Array: " + element );
+		}
 
-  }
+	}
 }
