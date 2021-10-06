f69b1a930c5ce367098205918bad713cb79164a9
NitaWasniowski <nita.wasniowski@brainsphere.de>
2021-10-06 16:35:23 +0200

    klasse umbenannt und ausgabe umbenannt

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,7 +1,7 @@
 
-public class Main {
+public class HelloWorld {
   public static void main(String[] args) {
   
-    System.out.println("Anderer Text");
+    System.out.println("Was anderes");
   }
 }
